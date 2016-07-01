454c59fc9f35abb5669a9d4e2a9342259ce103af
daniel258 <danielnevo@gmail.com>
2016-07-01 19:26:59 -0400

    Reorganized files

--- R/DupliData.R
+++ R/DupliData.R
@@ -1,7 +1,7 @@
 ######
-# 
+#
 #DupliData function expect a data frame, a single expousre (X), a mediator (M) and outcome (Y), and unless told other wise
-# it uses 
+# it uses
 # Set alter to be T if to want the duplicate observarions to be next ot each other
 # that is Id = 1 1 2 2 and so on
 #  - end data
@@ -9,9 +9,9 @@
 
 DupliData <- function(df, mediator = NULL, outcome = NULL, case = NULL , Time = NULL,  surv = F, ID = NULL)
 {
-  if (surv==T & (is.null(case) | is.null(Time))) {stop("For survival data, both case and time are needed")} 
-  #if (surv==F & (is.null(outcome) | is.null(Time))) {stop("For survival data, both case and time are needed")} 
-  
+  if (surv==T & (is.null(case) | is.null(Time))) {stop("For survival data, both case and time are needed")}
+  #if (surv==F & (is.null(outcome) | is.null(Time))) {stop("For survival data, both case and time are needed")}
+
   if (surv == F) {
   {
     n.row <- nrow(df)
@@ -19,7 +19,7 @@ DupliData <- function(df, mediator = NULL, outcome = NULL, case = NULL , Time = 
     df.star[[paste0(mediator)]] <- rep(0,n.row)
     names.covar <- names(df)[!(names(df) %in% c(outcome,mediator))]
     names(df.star)[!(names(df) %in% c(outcome,mediator))] <- paste0(names.covar,".star")
-    
+
     for (var.name in names.covar)
     {
       df[[paste0(var.name,".star")]]  <- rep(0,n.row)
@@ -37,9 +37,9 @@ DupliData <- function(df, mediator = NULL, outcome = NULL, case = NULL , Time = 
     {
 print("survival is yet to be included")
     }
-    
+
     df.return <- data.frame(IDdupl, model,  Xdupl1, Xdupl2, Mdupl, casedupl, Timedupl)
     }
-  
+
   return(df.dupl)
 }
